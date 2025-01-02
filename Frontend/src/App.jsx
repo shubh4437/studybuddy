bf3bb0bd1554f10b6d1a4cf715da41641ad0776b
Shubham <your_email@example.com>
2025-01-02 16:52:44 +0530

    notes card added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import Tools from "./components/Tools";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Tools />
       <Footer />
     </>
   );
---
+++ Frontend/src/components/Tools.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+function Tools() {
+  return (
+    <>
+      <div className="max-w-screen-2xl container flex flex-col  mx-auto px-6 py-2 my-0 md:py-2 md:px-20">
+        <div className="flex flex-col py-5 md:flex-row ">
+          <div className=" bg-base-100 image-full px-5 py-5 md:py-0 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://th.bing.com/th/id/OIP.KcKB0QgSKzEZ8MTtcbY5FwHaE7?w=265&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Stats-1&2</h2>
+              <p>Download latest Stats notes</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Download</button>
+              </div>
+            </div>
+          </div>
+          <div className=" bg-base-100 px-5 py-5 md:py-0 image-full w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://th.bing.com/th/id/OIP.I0ehhkeCT40IRNvecnEHSQHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Maths-1&2</h2>
+              <p>Download latest Maths notes</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Download</button>
+              </div>
+            </div>
+          </div>
+          <div className=" bg-base-100 image-full px-5 py-5 md:py-0 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://th.bing.com/th/id/OIP.OQM5s2z8OWEToD42RZOS0gHaEK?w=310&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">English-1&2</h2>
+              <p>Download latest English notes</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Download</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row ">
+          <div className=" bg-base-100 image-full px-5 py-5 md:py-0 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://th.bing.com/th/id/OIP.ixSKQxGAtH8D6WeohpoTeAHaEK?w=285&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Computational Thinking</h2>
+              <p>Download latest CT notes</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Download</button>
+              </div>
+            </div>
+          </div>
+          <div className=" bg-base-100 px-5 py-5 md:py-0 image-full w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://th.bing.com/th/id/OIP.ueJ50PRDhW6hdcSO1GiCWgHaEK?w=317&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Python Programming</h2>
+              <p>Download latest Python notes</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Download</button>
+              </div>
+            </div>
+          </div>
+          <div className=" bg-base-100 image-full px-5 py-5 md:py-0 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://th.bing.com/th/id/OIP.I0ehhkeCT40IRNvecnEHSQHaEK?w=304&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Subject</h2>
+              <p>Download latest</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary"><a href="" >Download</a></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Tools;
