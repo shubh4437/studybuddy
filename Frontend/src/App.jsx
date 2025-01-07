cc63ad76919849166ce4d09acc8b3cbfcf872409
Shubham <your_email@example.com>
2025-01-07 23:13:53 +0530

    slider added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
-import FreeNotes from "./components/freeNotes";
+import FreeNotes from "./components/FreeNotes";
 
 function App() {
   return (
--- Frontend/src/components/freeNotes.jsx
+++ Frontend/src/components/freeNotes.jsx
@@ -1,14 +1,89 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import list from "../list.json";
 
 function FreeNotes() {
   const filterData = list.filter((data) => data.category === "Free");
   console.log(filterData);
 
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div>
-      <h1>Free Notes</h1>
-    </div>
+    <>
+      <div className="max-w-screen-2xl container  mx-auto  md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2 ">Free Offered Notes</h1>
+          <p>
+            Providing concise, high-quality handwritten notes for Python, Math 1
+            & 2, Stats 1 & 2, English 1 & 2, and Computation Thinking. Perfect
+            for quick learning and exam preparation!
+          </p>
+        </div>
+        <div>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+            <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </>
   );
 }
 
