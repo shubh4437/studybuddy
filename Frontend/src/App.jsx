71f364a04d3e9d3d1feb27f23c804005c8650915
Shubham <your_email@example.com>
2025-01-16 01:42:04 +0530

    fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
-import FreeNotes from "./components/FreeNotes";
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
-      <FreeNotes />
+      {/* <FreeNotes /> */}
       <Footer />
     </>
   );
